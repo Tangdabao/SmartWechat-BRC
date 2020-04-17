4334b2ee97a6e40cf211e7443eebfbcd68985e0a
TylerDon <1813354283@qq.com>
2020-04-17 16:33:34 +0800

    添加系统参数界面

---
+++ pages/sysParameters/sysParameters.js
@@ -0,0 +1,76 @@
+/** 
+ *******************************************************************************
+ * @file     : sysParameters
+ * @details  ：系统参数配置
+ * @author   ：Tyler   
+ ******************************************************************************
+ */
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    customerName:"LYWY",//客户名称
+    customerID:"1234567890",//客户ID号
+    customerGroupID:"123456",//客户组ID
+    remainTime:"60",//剩余使用时长
+    forcedSwitchLightTime:"30"//强制开关灯时长
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
